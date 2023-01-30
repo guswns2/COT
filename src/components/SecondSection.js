a8ab886b7e3513a51f4ceb505b53a5e137eb574e
ChanHo01~ <chp9227@naver.com>
2023-01-30 14:34:56 +0900

    차노커밋

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -1,22 +1,46 @@
+import { TextField } from "@mui/material";
 import "./css/SecondSection.css";
 
 const SecondSection = () => {
   return (
-    <div className="second-section">
-      <div className="second-container1" id="info1-con2">
-        <div className="second-info1" id="Second Info1" />
-      </div>
-      <div className="second-container2" id="info1-con1">
-        <div className="second-info2" id="Second Info2" />
-        <div className="second-info3" id="info1-c1d2">
-          <div className="info3-box1" id="info1-c1d2" />
-          <div className="info3-box1" id="info1-c1d2" />
+    <section className="second-section" id="Second Section">
+      <div className="second-container1" id="Second Container1">
+        <div className="second-info1" id="Second Info1">
+          <div className="info1-box11" id="Info1 Box1">
+            <TextField
+              className="selectdate"
+              type="date"
+              InputLabelProps={{ shrink: true }}
+              variant="standard"
+              size="medium"
+              color="primary"
+              fullWidth
+            />
+            <button className="weekbutton" id="WeekButton">
+              <b className="b4">주간</b>
+            </button>
+            <button className="weekbutton" id="MonthButton">
+              <b className="b4">월간</b>
+            </button>
+          </div>
+          <div className="info1-box21" id="Info1 Box">
+            <button className="pdfbutton" />
+          </div>
         </div>
       </div>
-      <div className="second-container3" id="info1-con2">
-        <div className="second-info1" id="Second Info4" />
+      <div className="second-container2" id="Second Container2">
+        <div className="second-info2" id="Second Info2">
+          <div className="info2-box1" id="info2 Box1">
+            <b className="b6">기간별 전력소비량</b>
+          </div>
+        </div>
+        <div className="second-info2" id="Second Info3">
+          <div className="info2-box1" id="info3 box1">
+            <b className="b6">기간별 탄소배출량</b>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
